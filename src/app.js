aaf06ac43ca6b2be296580e98edef6fbd2cef122
Jenna Ransom <jenna.ransom@gmail.com>
2021-09-02 00:24:36 -0600

    adds automated movement and boundaries

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
+let positionX
+let positionY
+let speedX
+let speedY
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
+  positionX = 100
+  positionY = 200
+  speedX = 5
+  speedY = 5
 }
 
 function draw() {
   background("#fe934b44")
   fill("#ffffff")
   noStroke()
-  circle(mouseX, mouseY, 20)
+  circle(positionX, positionY, 20)
+  positionX += speedX
+  positionY += speedY
+  if ( positionY > windowHeight || positionY < 0) {
+    speedY *= -1
+  }
+  if (positionX > windowWidth || positionX < 0) {
+    speedX *= -1
+  }
 }
 
 function windowResized() {
