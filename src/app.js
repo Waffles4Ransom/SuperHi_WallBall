0d5e8a8533b8529f7ce1d0bc032513af05d740b5
Jenna Ransom <jenna.ransom@gmail.com>
2021-09-03 17:05:02 -0600

    fixes boudnaries so ball bounces at it's edge vs center, constrains ball to remain on screen upon sudden window resizing

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ let positionX
 let positionY
 let speedX
 let speedY
+let radius
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
@@ -9,21 +10,24 @@ function setup() {
   positionY = 200
   speedX = 5
   speedY = 5
+  radius = 20
 }
 
 function draw() {
   background("#fe934b44")
   fill("#ffffff")
   noStroke()
-  circle(positionX, positionY, 20)
+  circle(positionX, positionY, radius * 2)
   positionX += speedX
   positionY += speedY
-  if ( positionY > windowHeight || positionY < 0) {
+  if ( positionY > windowHeight - radius || positionY < radius) {
     speedY *= -1
   }
-  if (positionX > windowWidth || positionX < 0) {
+  if (positionX > windowWidth - radius || positionX < radius) {
     speedX *= -1
   }
+  positionX = constrain(positionX, radius, windowWidth - radius)
+  positionY = constrain(positionY, radius, windowHeight - radius)
 }
 
 function windowResized() {
